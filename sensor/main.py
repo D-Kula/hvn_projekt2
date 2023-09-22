bf13b56cacfff57922d7be92a47741ef3e190165
Deniz Kula <denizkula1298@gmail.com>
2023-09-22 08:19:20 +0200

    Added functionality to read config from json

--- sensor/main.py
+++ sensor/main.py
@@ -1,9 +1,19 @@
 import onewire
 import ds18x20
 import time
+import json
 import paho.mqtt.client as mqtt
 
-with open("config.json", "r") as config:
-    sdsd
+data = 0
+
+with open("config.json","r") as file:
+    data = file
+
+temperature_pin = data['pin']
+read_interval = data['interval']
+
+
+
+
 
 client = mqtt.Client()
