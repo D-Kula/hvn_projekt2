df898a2b4c8ef5da43d82069b092d104547129eb
Deniz Kula <denkul1298@gmail.com>
2023-09-23 17:37:26 +0200

    JSON test

--- sensor/main.py
+++ sensor/main.py
@@ -1,12 +1,23 @@
 import onewire
 import ds18x20
+import machine
 import time
 import json
 
-data = 0
-
 with open("config.json","r") as file:
+    global data
     data = file
 
-temperature_pin = data['pin']
+temperature_pin = machine.Pin(data['pin'])
 read_interval = data['interval']
+
+temp = ds18x20.DS18X20(onewire.OneWire(temperature_pin))
+
+roms = temp.scan()
+
+while True:
+    print('temperatures:', end=' ')
+    temp.convert_temp()
+    time.sleep_ms(read_interval)
+    for rom in roms:
+        print(temp.read_temp(rom), end=' ')
