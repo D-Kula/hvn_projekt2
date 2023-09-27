da64aed0b99b8526e2f23ced1701e4507badc735
Deniz Kula <denizkula1298@gmail.com>
2023-09-27 11:28:10 +0200

    Formatted & linted

--- sensor/main.py
+++ sensor/main.py
@@ -20,7 +20,6 @@ sensor_id = hex(int.from_bytes(roms[0], "little"))
 while True:
     temp.convert_temp()
     time.sleep_ms(read_interval)
-    # print(f"{unit_id[2:]}" + ' ' + f"{sensor_id[2:]}", end=' ')
-    for rom in roms:
-        print(f"{unit_id[2:]}" + " " + f"{hex(int.from_bytes(rom, 'little'))}", end=" ")
-        print(temp.read_temp(rom), end="\n")
+    print(f"{unit_id[2:]}" + " " + f"{sensor_id[2:]}", end=" ")
+    print(temp.read_temp(roms[0]), end="\n")
+
