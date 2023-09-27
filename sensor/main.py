bb2731e63845287dbd999778f80b6a29dfda04d4
Deniz Kula <denizkula1298@gmail.com>
2023-09-27 10:08:33 +0200

    Correctly formatted seral output

--- sensor/main.py
+++ sensor/main.py
@@ -1,23 +1,30 @@
 import time
 import machine
 import onewire, ds18x20
+import machine
+import onewire, ds18x20
 import json
 
 with open("config.json","r") as file:
     global data
     data = json.load(file)
+    global data
+    data = json.load(file)
 
+temperature_pin = machine.Pin(data['pin'])
 temperature_pin = machine.Pin(data['pin'])
 read_interval = data['interval']
 
 temp = ds18x20.DS18X20(onewire.OneWire(temperature_pin))
 
 roms = temp.scan()
+unit_id = hex(int.from_bytes(machine.unique_id(), 'little'))
+sensor_id = hex(int.from_bytes(roms[0], 'little'))
 
 while True:
     temp.convert_temp()
     time.sleep_ms(read_interval)
-    print('temperature:', end=' ')
+    #print(f"{unit_id[2:]}" + ' ' + f"{sensor_id[2:]}", end=' ')
     for rom in roms:
+        print(f"{unit_id[2:]}" + ' ' + f"{hex(int.from_bytes(rom, 'little'))}", end=' ')
         print(temp.read_temp(rom), end='\n')
-        json.load
